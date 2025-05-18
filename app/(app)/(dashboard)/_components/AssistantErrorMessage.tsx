78a2de9c35c2fbb3dd9073183b7180c2f19672cd
Arjun Kamboj <arjunkamboj87699@gmail.com>
2025-05-18 18:21:52 +0530

    fixed the dummy data import

--- app/(app)/(dashboard)/_components/AssistantErrorMessage.tsx
+++ app/(app)/(dashboard)/_components/AssistantErrorMessage.tsx
@@ -2,14 +2,11 @@ import React from "react";
 
 import MessageCard from "./sub/error-message-card";
 
-import { assistantMessages } from "@/dummydata";
-
 export default function Component() {
   return (
     <MessageCard
       showFeedback
       avatar="https://nextuipro.nyc3.cdn.digitaloceanspaces.com/components-images/avatar_ai.png"
-      message={assistantMessages[0]}
       status="failed"
     />
   );
